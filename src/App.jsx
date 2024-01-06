60858213c37233c794e27910fcf52f96d5af5c1e
Vhs4 <vhszera44444@gmail.com>
2024-01-06 14:41:56 -0300

    FEAT: User Profile page

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ import { UserStorage } from './UserContext';
 import NotFound from './components/NotFound';
 import User from './components/User/User';
 import ProtectedRoute from './components/Help/ProtectedRoute';
-import Photo from './components/Photo/Photo'
+import Photo from './components/Photo/Photo';
+import UserProfile from './components/User/UserProfile';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
                 <User />
               </ProtectedRoute>} />
             <Route path="photo/:id" element={<Photo />} />
+            <Route path="profile/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
