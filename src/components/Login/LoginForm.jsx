e292ffeaec8cfd4626e0e0709d56f35e2177a033
Vhs4 <vhszera44444@gmail.com>
2024-01-07 23:22:15 -0300

    FEAT: Ajustes finais

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -36,7 +36,7 @@ const LoginForm = () => {
           : (
             <Button>Entrar</Button>
           )}
-        <Error error={error}/>
+        <Error error={error && "Dados incorretos"}/>
       </form>
       <Link className={styles.perdeu} to="/login/forgot-password">Perdeu a senha?</Link>
       <div className={styles.cadastro}>
